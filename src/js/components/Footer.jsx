054fae11001fda604b88ee98e1f753f53a6e10ad
Sergio Cordoba <sergiocordoba246@gmail.com>
2025-12-07 19:49:55 +0000

    Inicio

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+
+export const Footer = () => {
+    
+    return (
+        <div className="fixed-bottom bg-dark text-white text-center py-3 mt-4">
+            Creado por Sergio Córdoba. © Derechos reservados. 2025
+        </div>
+    );
+}
---
+++ src/js/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar bg-primary-subtle mb-3">
+            <div className="container-fluid d-flex justify-content-center align-items-center">
+                <span className="px-2 py-1 border border-1 border-primary rounded-3 bg-white text-primary fw-bold fs-3 shadow-sm">
+                    To-Do-List
+                </span>
+            </div>
+        </nav>
+    );
+};
